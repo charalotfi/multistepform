213b1a291c012477ad1f0c56a1db9c8e38c625d5
charalotfi <charalotfi.nformatique@gmail.com>
2020-11-20 21:37:41 +0200

    create component<< MasterForm>>

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import MasterForm from './components/MasterForm';
+
+function App() {
+  return (
+    <div className="App">
+     <MasterForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MasterForm.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class MasterForm extends React.Component{
+    constructor(props){
+        super(props);
+         }
+   
+    render(){
+        return(
+            <div>
+               <h1>hello from master Form</h1>
+              </div>
+        )
+        
+    }
+}
+export default MasterForm;
